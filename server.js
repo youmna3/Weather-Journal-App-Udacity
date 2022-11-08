78850f8aae5bbd5130c5219ab76898094130e576
youmna3 <youmna54@gmail.com>
2022-11-08 23:56:43 +0200

    edit

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 // Start up an instance of app
 const app = express();
 //require dependences
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,23 +19,22 @@ app.use(cors());
 //app is runing on port
 const port = 3000;
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 //add GET route
-app.get('/getData',(req, res)=>{
-    res.send(projectData);
-})
-
-//add POST route
-app.post('/postData',(req, res)=>{
-    projectData.temp = req.body.temp
-    projectData.content= req.body.content
-    projectData.date = req.body.date
-    res.send(projectData);
+app.get("/getData", (req, res) => {
+  res.send(projectData);
 });
 
+//add POST route
+app.post("/postData", (req, res) => {
+  projectData.temp = req.body.temp;
+  projectData.content = req.body.content;
+  projectData.date = req.body.date;
+  res.send(projectData);
+});
 
 // Setup Server
-app.listen(port, ()=>{
-    console.log(`app is running on http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`app is running on http://localhost:${port}`);
 });
--- website/app.js
+++ website/app.js
@@ -1,80 +1,73 @@
 /* Global Variables */
-const apiKey = 'f1d3260bfc47846225a10b38eb15583b';
-const btn = document.querySelector('#generate');
-btn.addEventListener('click', function () {
-        try {
-            const zipCode = document.querySelector('#zip').value;
-            const userResponse = document.querySelector('#feelings').value;
-            if (!zipCode) {
-                alert('enter your zip code');
-                return;
-            }
-            getWeather(zipCode)
-                .then((tempDegree) => {
-                    postData(tempDegree, userResponse, newDate)
-                        .then(() => {
-                            updateUI();
-                        });
-                });
-
-        } catch (error) {
-            console.log('error');
-        }
-
+const apiKey = "f1d3260bfc47846225a10b38eb15583b";
+const btn = document.querySelector("#generate");
+btn.addEventListener("click", function () {
+  try {
+    const zipCode = document.querySelector("#zip").value;
+    const userResponse = document.querySelector("#feelings").value;
+    if (!zipCode) {
+      alert("enter your zip code");
+      return;
+    }
+    getWeather(zipCode).then((tempDegree) => {
+      postData(tempDegree, userResponse, newDate).then(() => {
+        updateUI();
+      });
     });
+  } catch (error) {
+    console.log("error");
+  }
+});
 
-    
-const getWeather= async (zipCode, key) => {
-    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
-    const res = await fetch (baseUrl);
-    const tempDegree = await res.json();
-   
-    
-   console.log(tempDegree.main.temp);
-   return tempDegree.main.temp;
-}
-const getData = async()=>{
-    const getDataRoute = await fetch('/getData');
-    try{
-        //convert json to JS
+const getWeather = async (zipCode, key) => {
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+  const res = await fetch(baseUrl);
+  const tempDegree = await res.json();
+
+  console.log(tempDegree.main.temp);
+  return tempDegree.main.temp;
+};
+const getData = async () => {
+  const getDataRoute = await fetch("/getData");
+  try {
+    //convert json to JS
     const data = await getDataRoute.json();
-    
+
     console.log(data);
     return data;
-    }catch(error){
-        console.log('errrr');
-    };
+  } catch (error) {
+    console.log("errrr");
+  }
 };
-
 
 const postData = async (tempDegree, userResponse, newDate) => {
-    const postRoute = await fetch('/postData', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body:JSON.stringify({
-            temp:tempDegree,
-            content:userResponse,
-            date:newDate,
-        })
-    });
+  const postRoute = await fetch("/postData", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      temp: tempDegree,
+      content: userResponse,
+      date: newDate,
+    }),
+  });
 };
-const updateUI = async ()=>{
-    //use fetch to get data from server
-    // use url from GET route
-    const dataToUpdateUi =await fetch ('/getData');
-    try{
-        //convert data from json
-        const convertData = await dataToUpdateUi.json();
-        console.log(convertData);
-        document.querySelector('#temp').innerHTML=convertData.temp;
-        document.querySelector('#date').innerHTML=convertData.date;
-        document.querySelector('#content').innerHTML=convertData.content;
-    }catch(err){
-        console.log('errrrrr');
-    }
-}
+const updateUI = async () => {
+  //use fetch to get data from server
+  // use url from GET route
+  const dataToUpdateUi = await fetch("/getData");
+  try {
+    //convert data from json
+    const convertData = await dataToUpdateUi.json();
+    console.log(convertData);
+    document.querySelector("#temp").innerHTML = convertData.temp;
+    document.querySelector("#date").innerHTML = convertData.date;
+    document.querySelector("#content").innerHTML = convertData.content;
+  } catch (err) {
+    console.log("errrrrr");
+  }
+};
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
